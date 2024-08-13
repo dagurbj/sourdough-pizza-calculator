baef22583ab81739308e8f594f8d0601e9fef5ec
Dagur Bjarnason <dbj@netcompany.com>
2024-08-13 11:30:45 +0200

    Logo and domain updated

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/sourdough-pizza-calculator/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
