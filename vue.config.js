4ffdf304223044369742cda609f82dbfa1047671
dagurbj <103303549+dagurbj@users.noreply.github.com>
2024-11-26 13:12:16 +0100

    New title (#1)
    * Better title
    
    * npm audit fix
    
    ---------
    
    Co-authored-by: Dagur Bjarnason <dbj@netcompany.com>

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Sourdough Pizza Calculator',
+    },
+  },
+});
