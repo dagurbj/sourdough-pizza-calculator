058d058f5e8b8b03cc4e2d4d61adad68c07ce2e8
Dagur Bjarnason <dbj@netcompany.com>
2026-07-21 11:22:56 +0200

    SEO updates after review

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
